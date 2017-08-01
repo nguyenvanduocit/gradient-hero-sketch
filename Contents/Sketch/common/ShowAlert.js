cfea8240f5f143fffe4f5abec783f88c8ed77be0
Duoc Nguyen <nguyenvanduocit@gmail.com>
2017-08-01 23:47:43 +0700

    remove source code

--- Contents/Sketch/common/ShowAlert.js
+++
@@ -1,6 +0,0 @@
-function showAlert(title, message) {
-  var alert = COSAlertWindow.new()
-  alert.setMessageText(title)
-  alert.setInformativeText(message)
-  alert.runModal()
-}
--- Contents/Sketch/main.js
+++
@@ -1,7 +0,0 @@
-import {makeColor, buildDialog} from './common'
-export default function (context) {
-  if (context.selection.length == 0) {
-    buildDialog('Fix Gradient', 'Select a shape with a gradient first!').runModal()
-    return
-  }
-}
